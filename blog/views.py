bb2cf323d8e642bf1d3b84bac8671256fd6a95d6
selinahsu <selinaraindrop@gmail.com>
2020-08-28 11:43:31 -0400

    part 3 templates

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+# mock data: represent eahc post with a dictionaries
+posts = [
+  {
+    'author': 'Selina Hsu',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2020',
+  }, 
+  {
+    'author': 'Corey Schafer',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2020',
+  }, 
+]
 
-# the home page also needs to be added to urls.py 
+# each page also needs to be added to urls.py 
 def home(request): 
-  return HttpResponse('<h1>Blog Home</h1>')
+  context = {
+    'posts': posts # pass in the list of dictionaries to the posts key
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return HttpResponse('<h1>Blog About</h1>')
+  return render(request, 'blog/about.html', {'title': 'About'})
